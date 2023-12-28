49e041c807fd6a2eac8314c3603f4341bd1c77ba
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2023-12-28 13:41:12 +0530

    added basic page layout

---
+++ components/FeedCard/index.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import React from 'react'
+import { BiMessageRounded } from "react-icons/bi";
+import { FaRegHeart, FaRetweet } from "react-icons/fa6";
+import { MdOutlineFileUpload } from 'react-icons/md';
+
+const FeedCard: React.FC = () => {
+    return (
+        <div className='border border-gray-600 border-r-0 border-l-0  border-b-0  p-5  hover:bg-[#181818]  cursor-pointer transition-all'>
+            <div className='grid grid-cols-12 gap-3 '>
+                <div className='col-span-1'>
+                    <Image className='rounded-full' src="https://media.licdn.com/dms/image/D4D35AQEMotckAv_0Pw/profile-framedphoto-shrink_400_400/0/1702315846874?e=1704312000&v=beta&t=BP1gda0qVG_WiTua1lMJNcKCXb8zXuPIcGFBfm5hd2w" alt="userPic" height={50} width={50} />
+                </div>
+                <div className='col-span-11 mb-1 px-1'>
+                    <h5>Omkar Jagtap</h5>
+                    <p>
+                        Expanding our search for a CS2 coach/retired CSGO Player with hands-on experience in competitive CS for
+                        @GodsReignIN If you bring
+                    </p>
+                    <div className="flex  justify-between mt-4 text-lg p-1">
+                        <div className='mr-4'>
+                            <BiMessageRounded />
+                        </div>
+                        <div>
+                            <FaRetweet />
+                        </div>
+                        <div>
+                            <FaRegHeart />
+                        </div>
+                        <div className='mr-4'>
+                            <MdOutlineFileUpload />
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeedCard
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter,Quicksand } from 'next/font/google'
+
+
+
+
+const inter = Inter({ subsets: ['latin'] })
+const Quicks = Quicksand({ subsets: ['latin'] })
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className={inter.className}>
+    <Component {...pageProps} />
+
+  </div>
 }
